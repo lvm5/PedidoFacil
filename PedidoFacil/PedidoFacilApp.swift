dc18cb64532e7821f5f2d9ecc25200800d72c6a0
Leandro Vansan de Morais <lvm5@icloud.com>
2025-03-12 12:37:20 -0300

    Initial Commit

---
+++ PedidoFacil/PedidoFacilApp.swift
@@ -0,0 +1,17 @@
+//
+//  PedidoFacilApp.swift
+//  PedidoFacil
+//
+//  Created by Leandro Morais on 2025-03-12.
+//
+
+import SwiftUI
+
+@main
+struct PedidoFacilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
